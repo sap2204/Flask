efabf6186ceccbac310d2f1d4548a29f69e31872
sap2204 <tbksap@gmail.com>
2024-06-17 20:10:33 +0300

    Setted flashed messages

--- flsite.py
+++ flsite.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, flash
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kjjhjhjkhskjjlkajiohrfjkjhrn'
 
 
 menu = [{"name": "Установка", "url": "install-flask"},
@@ -31,6 +32,10 @@ def about():
 @app.route("/contact", methods=['POST', 'GET'])
 def contact():
     if request.method == "POST":
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено!', category='success')
+        else:
+            flash('Ошибка отправки!', category='error')
         print(request.form)
     return render_template('contact.html', title="Обратная связь", menu=menu)
 
